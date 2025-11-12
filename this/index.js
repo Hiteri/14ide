435057dbe7d414acc1774406fad5f068d4e75307
Hide <154065036+Hiteri@users.noreply.github.com>
2025-11-12 12:06:12 -0300

    this method with Sky: COTL

---
+++ this/index.js
@@ -0,0 +1,20 @@
+// this = reference to the object where THIS is used
+//        (the object depends on the immediate context)      
+//        person.name = this.name
+
+const spirit1 = {
+    name: "Fainting Warrior",
+    place: "Golden Wasteland",
+    sayHello: function(){console.log(`Hi! I am the ${this.name}`)},
+    realm: function(){console.log(`I am from ${this.place}`)}
+}
+
+const spirit2 = {
+    name: "Waving Bellmaker",
+    place: "Isle of Dawn",
+    sayHello: function(){console.log(`Hi! I am the ${this.name}`)},
+    realm: function(){console.log(`I am from ${this.place}`)}
+}
+
+spirit1.realm();
+spirit2.realm();
