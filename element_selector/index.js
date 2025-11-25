48beae94b7851be635d74133f81095698a819771
Hide <154065036+Hiteri@users.noreply.github.com>
2025-11-25 12:45:21 -0300

    An element selector containing TAG NAME

---
+++ element_selector/index.js
@@ -0,0 +1,20 @@
+// element selectors = Methods used to target and manipulate HTML elements
+//                     They allow you to select one or multiple HTML elements
+//                     from the DOM (Document Object Model)
+
+// 1. document.getElementById()           // ELEMENT OR NULL
+// 2. document.getElementsClassName()     // HTML COLLECTION
+// 3. document.getElementsByTargetName()  // HTML COLLECTION
+// 4. document.querySelector()            // ELEMENT OR NULL
+// 5. document.querySelectorAll()         // NODELIST
+
+const h4Elements = document.getElementsByTagName("h4");
+const liElements = document.getElementsByTagName("li");
+
+Array.from(h4Elements).forEach(h4Element => {
+    h4Element.style.backgroundColor = "pink";
+});
+
+Array.from(liElements).forEach(liElement => {
+    liElement.style.backgroundColor = "lightBlue";
+});
