6494a3586c989bbb1a95444187ef6f897bb992e9
Hide <154065036+Hiteri@users.noreply.github.com>
2026-05-29 16:27:24 -0300

    Add intro function to greet characters

---
+++ function.py
@@ -0,0 +1,6 @@
+def intro(first_name, last_name):
+    print("Hello, my name is", first_name, last_name)
+
+intro("Geralt", "of Rivia")
+intro("Cirilla", "of Cintra")
+intro("Yennefer", "of Vengerberg")
