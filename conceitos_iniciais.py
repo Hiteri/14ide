39a715a1f8a2a5a46d955bdb9f17c261ae4628af
Hide <154065036+Hiteri@users.noreply.github.com>
2026-07-24 16:48:07 -0300

    Add files via upload

---
+++ conceitos_iniciais.py
@@ -0,0 +1,19 @@
+# num1 = 7 
+# num2 = 5
+
+# print(num1 + num2)
+# print(num1 - num2)
+# print(num1 *  num2)
+# print(num1 / num2)
+
+# nome = "Hide"
+# print("Olá, " + nome)
+
+# name = input("What's your name?: ")
+# print(name)
+
+# num1 = int(input("Digite um número: "))
+# num2 = int(input("Digite outro número: "))
+# soma = num1 + num2
+# print(f"{num1} + {num2} = {soma}")
+
---
+++ condicionais.py
@@ -0,0 +1,22 @@
+# age = int(input("What is your age?: "))
+# if age >= 18:
+#     print("You are free!")
+# else:
+#     print("You cannot.")
+
+# senha_correta = "cabadonordeste123"
+# tentativa_senha = input("Digite a sua senha: ")
+
+# if senha_correta == tentativa_senha:
+#     print("Acesso liberado!")
+# else:
+#     print("Acesso negado. Tente novamente!")
+
+nota = float(input("Digite sua nota: "))
+
+if nota >= 7:
+    print("Aprovado! ✅")
+elif nota >= 5:
+    print("Recuperação! ⚠")
+else:
+    print("Reprovado! ❌")
