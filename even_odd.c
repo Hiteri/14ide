2a3abe39388fb38931e9f758636f8124cc863bbc
Hide <154065036+Hiteri@users.noreply.github.com>
2026-08-05 15:46:19 -0300

    Add files via upload

---
+++ even_odd.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+// Write a program that prompts the user for an integer and checks whether it is even or odd. If the number is even, print the value 0, and if it is odd, print the value 1.
+
+int main(){
+    int number;
+
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+
+    if(number % 2 == 0){
+        printf("The number is 0");
+    }
+    else{
+        printf("The number is 1");
+    }
+
+    return 0;
+}
